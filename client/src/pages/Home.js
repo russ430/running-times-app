605280a4c4b006eb19d3eb34a6a8a7912fa2f7c4
Alex <alexjrussian@gmail.com>
2020-02-17 14:45:54 -0500

    added cards for times in database rendered to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,55 @@
 import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+import { Grid } from 'semantic-ui-react';
+
+import TimeCard from '../components/TimeCard';
 
 function Home() {
+  const { loading, data } = useQuery(FETCH_POSTS_QUERY);
+
+  if(data) {
+    console.log(data.getTimes);
+  }
+
   return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
+    <Grid columns={3} divided>
+      <Grid.Row>
+        <h1>Recent Times</h1>
+      </Grid.Row>
+      <Grid.Row>
+        {loading ? <h1>Loading times...</h1> : (data.getTimes.map(time => (
+          <Grid.Column key={time.id} style={{ marginBottom: '20px' }}>
+            <TimeCard data={time} />
+          </Grid.Column>
+        )))}
+      </Grid.Row>
+    </Grid>
   )
 };
+
+const FETCH_POSTS_QUERY = gql`
+{
+    getTimes{
+      id
+      body
+      miles
+      time
+      createdAt
+      username
+      likeCount
+      likes {
+        username
+      }
+      comments {
+        id
+        username
+        createdAt
+        body
+      }
+      commentCount
+  }
+}
+`;
 
 export default Home;
