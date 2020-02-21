6e4864b69460f27df2690c810dc654fc127fb991
Alex <alexjrussian@gmail.com>
2020-02-21 17:01:36 -0500

    rearranged home page when logged in

--- client/src/components/Profile/PersonalBests/ListItem.js
+++ client/src/components/Profile/PersonalBests/ListItem.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { List } from 'semantic-ui-react';
 
-function listItem(props) {
+function listItem({ children, descriptor, label }) {
 
   return (
     <List.Item>
       <List.Icon size="small" name="trophy" color="yellow"/>
       <List.Content>
-        <List.Header>{props.children} {props.descriptor && `${props.descriptor}`}</List.Header>
-        <List.Description>{props.label}</List.Description>
+        <List.Header>{children} {descriptor && `${descriptor}`}</List.Header>
+        <List.Description>{label}</List.Description>
       </List.Content>
     </List.Item>
   )
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -15,47 +15,43 @@ function Home() {
 
   return (
     <>
+    <Grid centered columns={2}>
       {user && (
-          <Grid centered columns={2}>
-            <Grid.Column  width={5}>
+          <Grid.Column width={4}>
+            <Grid.Row>
               <PostForm />
-            </Grid.Column>
-            <Grid.Column width={6}>
-              <Card fluid>
+            </Grid.Row>
+            <Grid.Row>
+              <Card style={{ marginTop: '2rem' }}>
                 <Card.Content>
-                  <Grid centered columns={2}>
-                    <Grid.Column stretched>
-                      <h2 style={{ textAlign: 'center' }}>My Stats</h2>
-                      <div style={{ margin: '0 auto'}}>
-                        <BasicStats username={user.username}/>
-                      </div>
-                    </Grid.Column>
-                    <Grid.Column stretched>
-                      <h2 style={{ textAlign: 'center' }}>Personal Bests</h2>
-                      <PersonalBests home username={user.username}/>
-                    </Grid.Column>
-                  </Grid>
+                  <h2 style={{ textAlign: 'center', marginBottom: '1.5rem' }}>Stats</h2>
+                  <BasicStats username={user.username}/>
+                  <h2 style={{ textAlign: 'center', marginBottom: '1.5rem' }}>Personal Bests</h2>
+                  <PersonalBests home username={user.username}/>
                 </Card.Content>
               </Card>
-            </Grid.Column>
-          </Grid>
+            </Grid.Row>
+          </Grid.Column>
         )}
-        <Grid columns={3}>
-          <Grid.Row className="page-title">
-            <h1>Recent Times</h1>
-          </Grid.Row>
-          <Grid.Row >
-            {loading ? <h1>Loading times...</h1> : (
-              <Transition.Group duration={200}>
-                {data.getTimes.map(time => (
-                  <Grid.Column key={time.id} style={{ marginBottom: '20px' }}>
-                    <TimeCard type="home" data={time} />
-                  </Grid.Column>
-                ))}
-              </Transition.Group>
-            )}
-          </Grid.Row>
-         </Grid>
+        <Grid.Column width={12}>
+          <Grid columns={3}>
+            <Grid.Row className="page-title">
+              <h1>Recent Times</h1>
+            </Grid.Row>
+            <Grid.Row >
+              {loading ? <h1>Loading times...</h1> : (
+                <Transition.Group duration={200}>
+                  {data.getTimes.map(time => (
+                    <Grid.Column key={time.id} style={{ marginBottom: '20px' }}>
+                      <TimeCard type="home" data={time} />
+                    </Grid.Column>
+                  ))}
+                </Transition.Group>
+              )}
+            </Grid.Row>
+          </Grid>
+        </Grid.Column>
+      </Grid>
     </>
   )
 };
