7da5e81596d90472a35f3d7f47823676af7d1b94
Alex <alexjrussian@gmail.com>
2020-05-05 16:26:11 -0400

    updated and refactored old code

---
+++ README.md
@@ -0,0 +1,25 @@
+# We Run Here
+
+A social networking web ap for runners! Input your recent running times and they will be posted to a live feed on your home page. After posting your time your stats will be updated and shown on your profile along with any personal bests that you've achieved.
+
+## Live Demo
+
+[WeRunHere](https://bit.ly/we-run-here)
+
+## Built With
+
+* [React](http://reactjs.org/) - Javascript library
+* [GraphQL](https://graphql.org)
+* [Apollo](https://apollographql.com)
+* [NodeJs](https://nodejs.org)
+* [semantic-ui](https://react.semantic-ui.com/) - Used for styling
+* [styled-components](https://styled-components.com/) - Also used for styling
+
+## Authors
+
+* **Alex Russian**
+
+
+## License
+
+MIT 
--- index.js
+++ index.js
@@ -3,20 +3,28 @@ const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
-const { MONGODB } = require('./config');
+// const { MONGODB } = require('./config');
+
+const PORT = process.env.PORT || 5000;
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: ({ req }) => ({ req })
+  context: ({ req }) => ({ req }),
 });
 
 mongoose
-  .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
     console.log('MongoDB Connected');
-    return server.listen({ port: 5000 });
+    return server.listen(PORT);
   })
   .then(res => {
     console.log(`Server running at ${res.url}`);
+  })
+  .catch(err => {
+    console.log(err);
   });
